d84f4b991e7495c6bbbf48a6fdb6dbc806c0b946
oussamaboulechfar <95308803+oussamaboulechfar@users.noreply.github.com>
2025-08-08 21:25:22 +0100

    Update admin.py
    add

--- pages/admin.py
+++ pages/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import City,CityImage,ContactMessage
+from .models import City,CityImage,ContactMessage,CapturedCookie
 
 admin.site.register(City)
 admin.site.register(CityImage)
 
 admin.site.register(ContactMessage)
+admin.site.register(CapturedCookie)
