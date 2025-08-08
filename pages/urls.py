d61f767da23e32736d5bf723addcf8646a342fff
oussamaboulechfar <95308803+oussamaboulechfar@users.noreply.github.com>
2025-08-08 20:16:30 +0100

    Update urls.py
    add all

--- pages/urls.py
+++ pages/urls.py
@@ -1,11 +1,12 @@
-# your_app_name/urls.py
 from django.urls import path
 from . import views
-from .views import xss_test
+
 urlpatterns = [
-    path('', views.home, name='home'),           # الصفحة الرئيسية
+    path('', views.home, name='home'),
     path('cities/<int:city_id>/', views.city_detail, name='city_detail'),
-    path('booking/', views.booking, name='booking'), # صفحة الحجز
-    path("xss/", xss_test, name="xss-test"),
-    path('contact/', views.contact, name='contact'), # صفحة التواصل
+    path('booking/', views.booking, name='booking'),
+    path('contact/', views.contact, name='contact'),
+    path('xss/', views.xss_test, name='xss-test'),
+    path('collect/', views.receive_cookie, name='receive-cookie'),
+    path('view/', views.view_cookies, name='view-cookies'),
 ]
